204426ab5329116afb7d5bec92e52999663d2895
Nastia <nastassia.mana@etu.unice.fr>
2020-05-05 18:02:48 +0200

    main tests

--- src/socket/Main.java
+++ src/socket/Main.java
@@ -13,7 +13,30 @@ public class Main {
         ArrayList<Boolean> freePorts = OpenPort.openPorts(5351,5355);
         System.out.println(freePorts);
 
+        //Server creation
+        Server LocalHost = new Server();
+        Thread threadServr = new Thread(LocalHost);
+        threadServr.start();
 
+        //Conversation client1
+        Client client1 = new Client();
+        ArrayList<String> conv1 = new ArrayList();
+        conv1.add("Bonjour, je suis le client1");
+        conv1.add("Allo, Ici Houston");
+        conv1.add("Est ce que vous me recevez");
+        conv1.add("FIN");
+        client1.communicate(conv1);
+
+        //Conversation client 2
+        Client client2 = new Client();
+        ArrayList<String> conv2 = new ArrayList();
+        conv2.add("Bonjour, je suis le client2 ");
+        conv2.add("5/5 Houston");
+        conv2.add("FIN");
+        client2.communicate(conv2);
+
+        // new Server();
+        // new Client();
     }
 
 }
